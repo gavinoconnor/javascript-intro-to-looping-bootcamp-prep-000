e00659632cef9be9ca2621ac7f7ae72458c3cd7b
Gavin O'Connor <gedmund@gmail.com>
2019-03-20 00:07:22 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop(myArray) {
+  for (let i = 0; i < 25; i++)
+    if (i === 1) {
+      myArray.push('I am 1 strange loop.')
+    } else {
+      myArray.push(`I am ${i} strange loops.`)
+    }
+    return myArray
+}
+
+function whileLoop(n) {
+    break
+  }
+}
