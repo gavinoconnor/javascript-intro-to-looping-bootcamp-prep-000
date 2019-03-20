975290082c8114a215708b40374d18aac34609a4
Gavin O'Connor <gedmund@gmail.com>
2019-03-20 00:46:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,13 +14,13 @@ function whileLoop(n) {
   }
   return 'done'
 }
-var num = 0;
-function incrementVariable() {
-  num = num + 1;
-  return num;
-}
 
 function doWhileLoop(num) {
+  var num = 0;
+  function incrementVariable() {
+    num = num + 1;
+    return num;
+  }
   do {
     console.log('I run once regardless');
     while (incrementVariable() < num);
